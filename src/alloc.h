d9a797805e51d3dd7e1944f3badf539ad0b1a068
Thomas Mergener <mergener.thomas@gmail.com>
2020-10-03 19:07:39 -0300

    Added stack module and expanded allocation interface.

--- src/alloc.h
+++ src/alloc.h
@@ -1,8 +1,21 @@
 #ifndef ALLOC_H
 #define ALLOC_H
 
+#include "defines.h"
+
 #include <stdlib.h>
 
-void* Mon_Alloc(size_t s);
+C_LINKAGE_BEGIN
+
+typedef void* (MON_CALL *Mon_AllocFuncT)(size_t);
+typedef void* (MON_CALL *Mon_AllocZeroFuncT)(size_t count, size_t size);
+typedef void* (MON_CALL *Mon_ReallocFuncT)(void*, size_t);
+typedef void  (MON_CALL *Mon_FreeFuncT)(void*);
+
+MON_API void* MON_CALL Mon_Alloc(size_t s);
+MON_API void* MON_CALL Mon_Realloc(void* oldMem, size_t s);
+MON_API void  MON_CALL Mon_Free(void* mem);
+
+C_LINKAGE_END
 
 #endif // ALLOC_H
--- src/error.h
+++ src/error.h
@@ -1,18 +1,21 @@
 #ifndef MON_ERROR_H
 #define MON_ERROR_H
 
+#include "defines.h"
+
 enum {
 
 	MON_SUCCESS = 0,
 	MON_ERR_NOMEM,
 	MON_ERR_FILENOTFOUND,
 	MON_ERR_SYNTAX,
-	MON_ERR_BAD_ARG
+	MON_ERR_BAD_ARG,
+	MON_ERR_EMPTY_COLLECTION
 
 };
 
 typedef int Mon_RetCode;
 
-void Mon_Fatal(Mon_RetCode code);
+MON_API void MON_CALL Mon_Fatal(Mon_RetCode code);
 
 #endif // MON_ERROR_H
