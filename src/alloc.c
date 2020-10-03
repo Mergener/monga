cd9d2b04219a5b9f5d9a71f6e896813e36f186d2
Thomas Mergener <mergener.thomas@gmail.com>
2020-10-03 19:41:45 -0300

    Added support for custom allocators.

--- src/alloc.c
+++ src/alloc.c
@@ -1,11 +1,59 @@
 #include "alloc.h"
 
+#include <stdbool.h>
+
 #include "error.h"
 
-void* Mon_Alloc(size_t size) {
-	void* mem = malloc(size);
-	if (mem == NULL) {
-		Mon_Fatal(MON_ERR_NOMEM);
+static bool s_AllocatorSet = false;
+
+static Mon_Allocator s_Allocator;
+
+static void UseDefaultAllocator() {
+	Mon_Allocator allocator;
+
+	allocator.alloc = malloc;
+	allocator.allocZero = calloc;
+	allocator.free = free;
+	allocator.realloc = realloc;
+
+	Mon_SetAllocator(allocator);
+}
+
+bool Mon_SetAllocator(Mon_Allocator allocator) {
+	if (allocator.alloc == NULL ||
+	    allocator.allocZero == NULL ||
+		allocator.free == NULL ||
+		allocator.realloc == NULL) {
+		return false;
 	}
-	return mem;
+
+	s_AllocatorSet = true;
+
+	s_Allocator = allocator;
+
+	return true;
+}
+
+void* Mon_Alloc(size_t s) {
+	if (!s_AllocatorSet) {
+		UseDefaultAllocator();
+	}
+
+	return s_Allocator.alloc(s);
+}
+
+void* Mon_Realloc(void* oldMem, size_t s) {
+	if (!s_AllocatorSet) {
+		UseDefaultAllocator();
+	}
+
+	return s_Allocator.realloc(oldMem, s);
+}
+
+void Mon_Free(void* mem) {
+	if (!s_AllocatorSet) {
+		UseDefaultAllocator();
+	}
+
+	return s_Allocator.free(mem);
 }
--- src/alloc.h
+++ src/alloc.h
@@ -4,14 +4,34 @@
 #include "defines.h"
 
 #include <stdlib.h>
+#include <stdbool.h>
 
 C_LINKAGE_BEGIN
 
-typedef void* (MON_CALL *Mon_AllocFuncT)(size_t);
-typedef void* (MON_CALL *Mon_AllocZeroFuncT)(size_t count, size_t size);
-typedef void* (MON_CALL *Mon_ReallocFuncT)(void*, size_t);
-typedef void  (MON_CALL *Mon_FreeFuncT)(void*);
+typedef struct {
 
+	/** Default allocation function. Defaults to malloc. */
+	void* (*alloc)(size_t);
+
+	/** Zero-initialized allocation function. Defaults to calloc. */
+	void* (*allocZero)(size_t, size_t);
+
+	/** Memory reallocation function. Defaults to realloc. */
+	void* (*realloc)(void*, size_t);
+
+	/** Memory disposal function. Defaults to free. */
+	void  (*free)(void*);
+
+} Mon_Allocator;
+
+/**
+ *	Provides a custom allocator to Monga. The custom allocator's functions
+ *	will then be used by Monga's Mon_Alloc, Mon_Realloc and Mon_Free routines.
+ * 
+ * 	@return True if the allocator was accepted, false if one of its function pointers
+ * 	was NULL.
+ */ 
+MON_API bool MON_CALL Mon_SetAllocator(Mon_Allocator allocator);
 MON_API void* MON_CALL Mon_Alloc(size_t s);
 MON_API void* MON_CALL Mon_Realloc(void* oldMem, size_t s);
 MON_API void  MON_CALL Mon_Free(void* mem);
