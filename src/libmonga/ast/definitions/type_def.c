95118b70acedfd5fba317863252d6a8063333873
Thomas Mergener <mergener.thomas@gmail.com>
2020-10-13 14:37:30 -0300

    Typedesc and type_def (part of last commit)

--- src/libmonga/ast/definitions/type_def.c
+++ src/libmonga/ast/definitions/type_def.c
@@ -4,11 +4,14 @@
 
 #include "mon_alloc.h"
 #include "../../strutils.h"
+#include "ast/mon_typedesc.h"
 
 Mon_AstTypeDef* Mon_AstTypeDefNew(const char* name,
-                                  size_t nameLen) {
+                                  size_t nameLen,
+                                  Mon_AstTypeDesc* typeDesc) {
 
     MON_CANT_BE_NULL(name);
+    MON_CANT_BE_NULL(typeDesc);
 
 	Mon_AstTypeDef* ret = Mon_Alloc(sizeof(Mon_AstTypeDef));
 
@@ -23,15 +26,22 @@ Mon_AstTypeDef* Mon_AstTypeDefNew(const char* name,
     }
 
     ret->typeNameLength = nameLen;
+    ret->typeDesc = typeDesc;
 
     return ret;
 }
 
-void Mon_AstTypeDefDestroy(Mon_AstTypeDef* def) {
+void Mon_AstTypeDefDestroy(Mon_AstTypeDef* def, bool rec) {
     if (def == NULL) {
         return;
     }
 
+    Mon_AstTypeDef defVal = *def;
+
 	Mon_Free(def->typeName);
 	Mon_Free(def);
+
+    if (rec) {
+        Mon_AstTypeDescDestroy(defVal.typeDesc, true);
+    }
 }
